78c27dead472ff0d2200b4412d551309c6998c32
Jan Murre <jan.murre@gmail.com>
2020-02-12 20:21:45 +0100

    Added some comments to the publishing script.

--- static-files.sh
+++ static-files.sh
@@ -15,6 +15,7 @@ mkdir -p $TARGET/datasets
 git clone -q https://github.com/Amsterdam/amsterdam-schema.git
 cd amsterdam-schema
 
+# Fetch all tags, filter out unwanted path names, write out content with @version postfix in filename
 git tag -l 'v*' | while read version; do
   git ls-tree -r --name-only $version | grep "\.json\$" | grep -v "^test" | grep -v "^package" | while read path; do
     name=${path%.*}
@@ -24,6 +25,7 @@ git tag -l 'v*' | while read version; do
   done
 done
 
+# Same for HEAD, does not get @version postfix
 git ls-tree -r --name-only HEAD | grep "\.json\$" | grep -v "^test" | grep -v "^package" | while read path; do
   name=${path%.*}
   dir=$(dirname $path)
@@ -35,6 +37,7 @@ cd ..
 git clone -q https://github.com/Amsterdam/schemas.git
 cd schemas
 
+# Finally write out the published dataset from the schemas repo.
 git ls-tree -r --name-only HEAD | grep "\.json\$" | grep -v "^test" | grep -v "^package" | while read path; do
   name=${path%.*}
   dir=$TARGET/datasets/$(dirname $name)
